24b96494a273d1660d99f144774f19c473af044b
viettrung2103 <viettrung21@gmail.com>
2023-02-17 00:01:00 +0200

    refractor wa promise

--- B12-js-async/bt.js
+++ B12-js-async/bt.js
@@ -10,6 +10,7 @@
 //   phepChia(phepNhan(phepCong(a, b), h));
 // };
 
+//callback function
 const phepCong = (a, b, cb) => {
   console.log(`Start cong`);
   setTimeout(() => {
@@ -46,22 +47,6 @@ const phepChia = (a, b, cb) => {
   }, 3000);
 };
 
-// const phepNhan = (h) => {
-//   console.log(`phep nhan`);
-//   setTimeout(() => {
-//     let result = phepCong(a, b);
-//     let result1 = result * h;
-//     console.log(`Phep Nhan: ${result} + ${h} = ${result1}`);
-//     // cb(result);
-//   }, 2000);
-// };
-// const phepChia = (cb) => {
-//   setTimeout((result) => {
-//     let final = result / 2;
-//     console.log(`Ket qua: ${result}`);
-//   }, 1000);
-// };
-
 const dienTichHinhThang = (a, b, h) => {
   phepCong(a, b, (ketquaSum) => {
     phepNhan(ketquaSum, h, (ketquaMul) => {
@@ -92,10 +77,164 @@ const phepTinh = (a, b, c, d, e, f, h) => {
     });
   });
 };
-phepTinh(5, 6, 3, 4, 2, 5, 10);
+
+//promise function
+const congPromise = (a, b) => {
+  return new Promise((resolve, reject) => {
+    console.log(`Start cong Promise`);
+    const sum = a + b;
+    if (sum) {
+      setTimeout(() => {
+        console.log(` Phep Cong: ${a} + ${b} = ${sum} `);
+        resolve(sum);
+      }, 1000);
+    } else {
+      reject(Error("Khong cong duoc"));
+    }
+  });
+};
+
+const truPromise = (a, b) => {
+  return new Promise((resolve, reject) => {
+    console.log(`Start Tru Promise`);
+    const minus = a - b;
+    if (minus) {
+      setTimeout(() => {
+        console.log(` Phep Tru: ${a} - ${b} = ${minus} `);
+        resolve(minus);
+      }, 500);
+    } else {
+      reject(Error("Khong tru duoc"));
+    }
+  });
+};
+
+const nhanPromise = (a, b) => {
+  return new Promise((resolve, reject) => {
+    console.log(`Start Nhan Promise`);
+    let mul = a * b;
+    if (mul) {
+      setTimeout(() => {
+        console.log(` Phep Nhan: ${a} * ${b} = ${mul} `);
+        resolve(mul);
+      }, 2000);
+    } else {
+      reject(Error("Khong Nhan duoc"));
+    }
+  });
+};
+
+const chiaPromise = (a, b) => {
+  return new Promise((resolve, reject) => {
+    console.log(`Start Chia`);
+    let div = a / b;
+    if (b !== 0) {
+      setTimeout(() => {
+        console.log(` Phep Chia: ${a} / ${b} = ${div} `);
+        resolve(div);
+      }, 3000);
+    } else reject(Error("Khong the chia voi so 0"));
+  });
+};
+
+const phepTinhPromise = (a, b, c, d, e) => {
+  congPromise(a, b)
+    .then((kqCongPro1) => {
+      return truPromise(kqCongPro1, c);
+    })
+    .then((kqTruPromise1) => {
+      return nhanPromise(kqTruPromise1, d);
+    })
+    .then((kqNhanPromise) => {
+      return chiaPromise(kqNhanPromise, e);
+    })
+    .then((kqChiaPromise1) => {
+      console.log(`ket qua cuoi: ${kqChiaPromise1}`);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+// phepTinhNCPromise(3, 5, 6, 4, 5, 3, 2);
+
+// (a + b) * h * (c/ d) + (e- f) /2
+const phepTinhNCPromise = (a, b, c, d, e, f, h) => {
+  Promise.all([congPromise(a, b), chiaPromise(c, d), truPromise(e, f)])
+    // .then((kqArr) => {
+    //   return kqArr; // [(a+b), (c/d),(e-f)]
+    // })
+    .then((kqArr) => {
+      let kqNhanNCPro2 = nhanPromise(kqArr[0], h);
+      kqNhanNCPro2
+        .then((kqNhanPro2) => {
+          kqArr.push(kqNhanPro2);
+          return kqArr; //[(a+b), (c/d),(e-f), (a+b)*h]
+        })
+        .then((kqArr) => {
+          let kqNhanNCPro3 = nhanPromise(kqArr[3], kqArr[1]);
+          kqNhanNCPro3
+            .then((kqNhanPro3) => {
+              kqArr.push(kqNhanPro3);
+              return kqArr; // [(a+b), (c/d),(e-f), (a+b)*h,(a+b) * h * (c/d)]
+            })
+            .then((kqArr) => {
+              let kqCongNCPro2 = congPromise(kqArr[4], kqArr[2]);
+              kqCongNCPro2
+                .then((kqCongPro2) => {
+                  kqArr.push(kqCongPro2);
+                  return kqArr; // [(a+b), (c/d),(e-f), (a+b)*h,(a+b) * h * (c/d), (a+b) * h * (c/d) +(e-f)]
+                })
+                .then((kqAr) => {
+                  let kqChiaNCPro2 = chiaPromise(kqAr[5], 2);
+                  kqChiaNCPro2.then((result) =>
+                    console.log(`ket qua cuoi cung: ${result}`)
+                  );
+                });
+            });
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// congPromise(a, b)
+//   .then((kqCongPro1) => {
+//     return truPromise(kqCongPro1, c);
+//   })
+//   .then((kqTruPromise1) => {
+//     return nhanPromise(kqTruPromise1, d);
+//   })
+//   .then((kqNhanPromise) => {
+//     return chiaPromise(kqNhanPromise, e);
+//   })
+//   .then((kqChiaPromise1) => {
+//     console.log(`ket qua cuoi: ${kqChiaPromise1}`);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+phepTinhNCPromise(3, 5, 6, 4, 5, 3, 2, 5);
+// ((a + b -c) * d)/e
+// phepTinhPromise(3, 5, 6, 4, 5);
+// const phepTinhPromise (a,b,c) => {
+//   phep
+// }
+// new Promise((resove, reject) => {
+//   console.log(`Start cong`);
+//   setTimeout(() => {
+//     const sum = a + b;
+//     console.log(` Phep Cong: ${a} + ${b} = ${sum} `);
+//   }, 1000);
+//   if (sum) {
+//     resove;
+//   } else {
+//     reject();
+//   }
+// });
+
+// phepTinh(5, 6, 3, 4, 2, 5, 10);
 // callback hell: rat kho maintain
 // sinh ra 2 cái để xử lý bất đồng bộ: Promise, Async await
 
 // xu ly bai tap tren theo dang promise
-
-
